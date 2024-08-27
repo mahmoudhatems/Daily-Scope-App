b060c66dff230dade36da48e8856133654ad4e8c
mahmoud hatem <mahmoudhatem11220@gmail.com>
2024-08-27 07:07:14 +0300

    update

--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -9,7 +9,10 @@ class NewsServices {
    {
     var  response = await dio.get("https://newsapi.org/v2/top-headlines?country=eg&apiKey=8739d2128ad74ac18056c9c64275d2da&category=general");
      Map <String,dynamic> jsonData= response.data;
-print(jsonData);
+List<dynamic> articles=jsonData["articles"];
+print(articles);
+
+
   }
   
 
