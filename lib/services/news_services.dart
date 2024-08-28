b7106205471c4f755bf4c038ab3c6b13d1438d17
mahmoud hatem <mahmoudhatem11220@gmail.com>
2024-08-28 05:31:37 +0300

    Update news services

--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -1,19 +1,18 @@
+import 'package:daily_scope/models/article_model.dart';
 import 'package:dio/dio.dart';
 
 class NewsServices {
-  final Dio  dio;
+  final Dio dio;
 
   NewsServices({required this.dio});
 
-  getNews() async
-   {
-    var  response = await dio.get("https://newsapi.org/v2/top-headlines?country=eg&apiKey=8739d2128ad74ac18056c9c64275d2da&category=general");
-     Map <String,dynamic> jsonData= response.data;
-List<dynamic> articles=jsonData["articles"];
-print(articles);
-
-
+  getNews() async {
+    var response = await dio.get(
+        "https://newsapi.org/v2/top-headlines?country=eg&apiKey=8739d2128ad74ac18056c9c64275d2da&category=general");
+    Map<String, dynamic> jsonData = response.data;
+    List<dynamic> articles = jsonData["articles"];
+    
+    List <ArticleModel>articleList=[];
+     
   }
-  
-
 }
