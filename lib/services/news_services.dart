fc35806ed016b51e789247ce52dc3f887195ddd7
mahmoud hatem <mahmoudhatem11220@gmail.com>
2024-09-06 23:06:32 +0300

    update

--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -7,25 +7,32 @@ class NewsServices {
   NewsServices({required this.dio});
 
   Future<List<ArticleModel>> getNews() async {
-    var response = await dio.get(
-        "https://newsapi.org/v2/top-headlines?country=us&apiKey=8739d2128ad74ac18056c9c64275d2da&category=general");
-    Map<String, dynamic> jsonData = response.data;
-    List<dynamic> articles = jsonData["articles"];
+     return await Future.delayed(const Duration(seconds: 2), () async {
+      var response = await dio.get(
+          "https://newsapi.org/v2/top-headlines?country=us&apiKey=8739d2128ad74ac18056c9c64275d2da&category=general");
+      Map<String, dynamic> jsonData = response.data;
+      List<dynamic> articles = jsonData["articles"];
 
-    List<ArticleModel> articleList = [];
-    for (var article in articles) {
-      ArticleModel articleModel = ArticleModel(
-        image: article["urlToImage"] ?? '', // Provide a default value if null
-        title: article["title"] ?? 'No Title', // Provide a default title if null
-        description: article["description"] ?? 'No Description', // Provide a default description if null
-        subTitle: '',
-        source: Source(
-          id: article['source']['id'] ?? 'unknown', // Provide a default id if null
-          name: article['source']['name'] ?? 'Unknown', // Provide a default name if null
-        ),
-      );
-      articleList.add(articleModel); // Don't forget to add the article to the list
-    }
-    return articleList;
+      List<ArticleModel> articleList = [];
+      for (var article in articles) {
+        ArticleModel articleModel = ArticleModel(
+          image: article["urlToImage"] ?? '', // Provide a default value if null
+          title:
+              article["title"] ?? 'No Title', // Provide a default title if null
+          description: article["description"] ??
+              'No Description', // Provide a default description if null
+          subTitle: '',
+          source: Source(
+            id: article['source']['id'] ??
+                'unknown', // Provide a default id if null
+            name: article['source']['name'] ??
+                'Unknown', // Provide a default name if null
+          ),
+        );
+        articleList
+            .add(articleModel); // Don't forget to add the article to the list
+      }
+      return articleList;
+    });
   }
 }
